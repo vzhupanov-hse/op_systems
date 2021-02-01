21dbd62bd16b35c28e105c0f88545aa893754f7f
vzhupanov-hse <vzhupanov@edu.hse.ru>
2021-02-01 14:21:08 +0300

    Lab 2 done.

---
+++ 03-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t p = fork();
+    if (p == 0)
+	printf("I'm child process with P = %d\nMy parent's P = %d\n", (int)getpid(), (int)getppid());
+    else if (p == -1)
+	printf("Something went wrong, can't create child process\n");
+    else
+    {
+	printf("I'm parent with PID = %d\nMy child's PID = %d\n", (int)getpid(), (int)pid);
+	sleep(1);
+    }
+    return 0;
+}
