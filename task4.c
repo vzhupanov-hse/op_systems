176256be3e0132f617c563ed2faadb98b5e41a67
vzhupanov-hse <72622028+vzhupanov-hse@users.noreply.github.com>
2021-02-14 17:52:29 +0300

    Update task4.c

--- task4.c
+++ task4.c
@@ -5,12 +5,12 @@
 int main () {
     pid_t pid, ppid;
     pid = fork();
-    if (pid == 0) {
-        printf("child with ppid: %d\n", getppid());
-        execl("./3", "arg1", "arg2", "arg3", "arg4", (char *)0);
-    } else if (pid == -1) {
+    if (pid == -1) {
         printf("cheers, you have an exception");
         return 1;
+    } else if (pid == 0) {
+        printf("child with ppid: %d\n", getppid());
+        execl("./3", "arg1", "arg2", "arg3", "arg4", (char *)0);
     } else {
         prinf("parent with pid: %d\n", getpid());
     }
