b95c987d84571501e7a1b2c34a117a76265199a1
vzhupanov-hse <72622028+vzhupanov-hse@users.noreply.github.com>
2021-02-22 16:41:40 +0300

    Update Task4-2.c

--- Task4-2.c
+++ Task4-2.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -8,24 +9,25 @@
 int main()
 {
     int fd;
-    int result;
+    int res;
     size_t size;
     char str_res[14];
-    char path[] = "text.fifo";
-
+    char path[] = "text.fifo"; 
     (void)umask(0);
+    if (mknod(path, S_IFIFO | 0666, 0) < 0) {
+        printf("file is not working\n");
+        exit(-1);
+    }
     if ((fd = open(path, O_RDONLY)) < 0) {
         printf("Can not open fifo for reading\n");
         exit(-1);
     }
     size = read(fd, str_res, 14);
     if (size < 0) {
-        printf("Can not read string from fifo\n");
+        printf("Can not reading\n");
         exit(-1);
     }
     printf("Result: %s\n", str_res);
-    if (close(fd) < 0) {
-        printf("Can not close fifo\n"); exit(-1);
-    }
+    close(fd);
     return 0;
 }
